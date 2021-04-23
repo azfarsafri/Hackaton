90634274508a3f536cb9ff7525d77ba237d532c7
bramfunnf <bramadiputra15@gmail.com>
2021-04-23 10:06:28 +0700

    merge with adit

--- backend.js
+++ backend.js
@@ -1,71 +1,77 @@
 //variabel const
+//-----------------------------
+
 const ON_PROGREES = "ON PROGRESS";
 const ON_THE_WAY = "ON_THE_WAY";
 const DELIVERED = "DELIVERED";
+const IMG_ON_THE_WAY = "";
+const IMG_DELIVERED = "";
 
 // Data-Structure
+// ------------------------------
+
 let foods = [
     {
-        id: 1,
+        id: 0,
         name: "Burger Buto Ijo",
         image: "link",
-        price: 1000,
+        price: 25000,
+        qty: 0
+    },
+    {
+        id: 1,
+        name: "Burger Gunderuwo",
+        image: "link",
+        price: 23000,
         qty: 0
     },
     {
         id: 2,
-        name: "Burger Gunderuwo",
+        name: "Burger Pocong",
         image: "link",
-        price: 2000,
+        price: 21000,
         qty: 0
     },
     {
         id: 3,
-        name: "Burger Pocong",
+        name: "Hot Dog Gukguk",
         image: "link",
-        price: 3000,
+        price: 17000,
         qty: 0
     },
     {
         id: 4,
-        name: "Hot Dog Gukguk",
+        name: "Salad Sehat",
         image: "link",
-        price: 4000,
+        price: 17000,
         qty: 0
     },
     {
         id: 5,
-        name: "Salad Sehat",
+        name: "Kentang Tuyul",
         image: "link",
-        price: 5000,
+        price: 15000,
         qty: 0
     },
     {
         id: 6,
-        name: "Kentang Tuyul",
+        name: "Es Kuntilanak Merah",
         image: "link",
-        price: 6000,
+        price: 10000,
         qty: 0
     },
     {
         id: 7,
-        name: "Es Kuntilanak Merah",
+        name: "Cupcake Enak",
         image: "link",
-        price: 3000,
+        price: 7000,
         qty: 0
     },
     {
         id: 8,
-        name: "Cupcake Enak",
-        image: "link",
-        price: 5000,
-        qty: 0
-    },
-    {
-        id: 9,
         name: "Es Kuyang",
         image: "link",
-        price: 7000,
+        price: 10000,
         qty: 0
     }
 ]
@@ -76,7 +82,9 @@ let cart = {
     totalPrice: 0
 }
 
-//function
+//FUNCTION
+// -------------------------------------------------------
+
 // addCart -> berfungsi untuk menambahkan item food yang dibeli
 function addCart (arrFoods){
     for (let i = 0; i < arrFoods.length; i++){
@@ -92,8 +100,6 @@ function addCart (arrFoods){
         }
     }
 }
-// addCart(foods);
-// console.log(cart);
 
 // getCartInfo -> berfungsi untuk mendapatkan informasi cart.
 function getCartInfo(){
@@ -105,8 +111,8 @@ function getCartInfo(){
         console.log("price:",food.price * food.qty);
     }
     console.log("total harga: ", cart.totalPrice);
+    return cart;
 }
-// console.log(getCartInfo());
 
 // resetFoodQty -> berfungsi untuk mereset kuantiti setiap menu menjadi 0 atau default.
 function resetFoodQty(){
@@ -115,6 +121,35 @@ function resetFoodQty(){
         food.qty = 0;
     }
 }
-// console.log(foods);
-// resetFoodQty();
-// console.log(foods);
+
+// plusQuantity -> berfunsi untuk menambahkan qty saat tombol + dipencet
+function plusQuantity(id) {
+    foods[id].qty++
+}
+
+// minusQuantity -> berfunsi untuk mengurangkan qty saat tombol - dipencet
+function minusQuantity(id) {
+    if (foods[id]["qty"] === 0) {
+        alert("Tidak boleh memesan kurang dari 0")
+    } else {
+        foods[id].qty--
+    }
+}
+
+// getInfoFood -> berfunsi untuk mendapatkan object food berdasarkan idnya
+function getInfoFood(id) {
+    return foods[id]
+}
+
+// updateStatus -> berfunsi untuk update status dalam cart
+function updateStatus() {
+    if (cart.status === ON_PROGREES) {
+        console.log(ON_THE_WAY);
+        console.log("gambar");
+        cart.status = ON_THE_WAY;
+    } else if (cart.status === ON_THE_WAY) {
+        console.log(DELIVERED);
+        console.log("gambar Delivered");
+        cart.status = DELIVERED;
+    }
+}
